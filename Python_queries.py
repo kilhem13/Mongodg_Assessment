1e33bd1e91a760caa9e47e932bb245a0c7d2c539
guilhem_lr <guilhemlauro@gmail.com>
2019-12-07 15:38:49 +0100

    Query 3

--- Python_queries.py
+++ Python_queries.py
@@ -1,3 +1,4 @@
+#1
 import pymongo
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 mydb = myclient.company
@@ -5,6 +6,19 @@ mycol = mydb["employees"]
 pipeline = [
     { "$match": { "job": "clerk" } },
     { "$group": { "_id": "$job", "salary": { "$max": "$salary" } } },
+    { "$project": { "_id": 0 } }
+            ]
+list(mydb.employees.aggregate(pipeline))
+
+
+#3
+import pymongo
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+mydb = myclient.company
+mycol = mydb["employees"]
+pipeline = [
+    { "$group" : { "_id": "null", "max": { "$max" : "$salary" }, "low": { "$min" : "$salary" }, "avgSalary": { "$avg": "$salary" }}
+    },
     { "$project": { "_id": 0 } }
             ]
 list(mydb.employees.aggregate(pipeline))
